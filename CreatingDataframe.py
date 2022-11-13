26e5dda94f2056b75be3dd93eac5f28f9f27c887
Michelle Roberts <80293351+MichelleRobertsBU@users.noreply.github.com>
2022-11-13 15:35:31 -0500

    Created Table with Dataframe

--- CreatingDataframe.py
+++ CreatingDataframe.py
@@ -1,12 +1,11 @@
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 #read with headers
-df=pd.read_csv("NEO Earth Close Approaches (2).csv", nrows=10)
+df=pd.read_csv("NEO Earth Close Approaches (2).csv", )
 
-#display
 print(df)
-
-df=pd.read_csv("NEO Earth Close Approaches (2).csv")
 
 # Get the total number of rows
 rowCount = df.shape[0]
@@ -17,3 +16,36 @@ print(rowCount)
 columnCount = df.shape[1]
 print('Total columns are ')
 print(columnCount)
+
+#Get the mean of the The minimum possible close-approach distance
+print('The mean of the minimum possible close-approach distance (Earth center to NEO center), in kilometers is')
+print(df['CA DistanceMinimum (km)'].mean())
+
+#Get the maximum value of the The minimum possible close-approach distance
+print('The maximum value of the minimum possible close-approach distance (Earth center to NEO center), in kilometers in this list is')
+print(df['CA DistanceMinimum (km)'].max())
+
+#Get the minimum value of the The minimum possible close-approach distance
+print('The minimum value of the minimum possible close-approach distance (Earth center to NEO center), in kilometers in this list is')
+print(df['CA DistanceMinimum (km)'].min())
+
+# Creating a table with Dataframe
+
+##define figure and axes
+fig, ax = plt.subplots()
+
+##hide the axes
+fig.patch.set_visible(False)
+ax.axis('off')
+ax.axis('tight')
+
+##create Dataframe
+data = pd.read_csv("NEO Earth Close Approaches (2).csv", nrows=10, usecols=["Object","CA DistanceMinimum (km)", "Diameter"])
+df = pd.DataFrame(data)
+
+## Create table
+table = ax.table(cellText=df.values, colLabels=df.columns, colColours =["palegreen"] * 10, cellLoc ='center', loc='center')
+
+##display table
+fig.tight_layout()
+plt.show()
--- NearEarthAsteroid.py
+++ NearEarthAsteroid.py
@@ -1,4 +1,5 @@
 
+import colorsys
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,5 +10,29 @@ print(df)
 
 print(df.dtypes)
 
-df.plot(x = 'CA DistanceNominal (km)', y = 'Object')
-plt.show
+
+
+# Set the figure size
+plt.rcParams["figure.figsize"] = [12.00, 6.50]
+plt.rcParams["figure.autolayout"] = True
+
+# Make a list of columns
+columns = ['CA DistanceMinimum (km)','Object']
+
+# Read a CSV file
+df = pd.read_csv('NEO Earth Close Approaches (2).csv', usecols=columns)
+
+
+
+# Plot the lines
+colors = np.random.randint(43, size=(43))
+df.plot.scatter(x = 'Object', y = 'CA DistanceMinimum (km)', c=colors, cmap='nipy_spectral')
+
+#Add title and labels to x- and y-axis
+plt.suptitle('The minimum possible close-approach distance (Earth center to NEO center), in kilometers')
+plt.ylabel('Minimum Distance')
+plt.xlabel('Near Earth Object')
+#rotate x-axis tick labels
+plt.xticks(rotation=45)
+  
+plt.show()
