496ac424bae46f8919659a743d8df440b0d3fcdb
Michelle Roberts <80293351+MichelleRobertsBU@users.noreply.github.com>
2022-11-17 05:39:04 -0500

    Included Lunar distance chart

--- NearEarthAsteroid.py
+++ NearEarthAsteroid.py
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 import matplotlib.image as img
 
 #Downloaded CSV file on Near Earth Asteroid tracking to analyze the data in comparison
-to the Earth's Moon for interested observers. The data is from NASA public domain. See 
-the README for License information and key for the column information.
+#to the Earth's Moon for interested observers. The data is from NASA public domain. See 
+#the README for License information and key for the column information.
 ##Requirement (1) Read in data from a local csv file with Pandas read_functions.
 
 df = pd.read_csv('NEO_Earth_Close_Approaches.csv')
@@ -51,9 +51,9 @@ df=pd.read_csv("NEO_Earth_Close_Approaches.csv")
 print(df)
 
 #The following numpy functions were used to find out the number of rows, number of columns, mean, 
-standard deviation, maximum and minimum values of the close-approach distance.
+#standard deviation, maximum and minimum values of the close-approach distance.
 ##Requirement (3) Do 5 basic calculations with Pandas or use 5 different built-in Python functions
-to find out something about your data. 
+##to find out something about your data. 
 
 # Get the total number of rows
 rowCount = df.shape[0]
@@ -105,6 +105,31 @@ table.scale(1,2)
 fig.tight_layout()
 plt.show()
 
+#Moon distance comparison
+##define figure and axes
+fig, ax = plt.subplots()
+plt.suptitle('Comparison to the Moon distance of the ten closest Asteroids.', fontsize=15)
+
+##hide the axes
+fig.patch.set_visible(False)
+ax.axis('off')
+ax.axis('tight')
+
+#Dataframe of Near Earth Asteroids Minimum Distance in comparison to Moon's Distance from Earth.
+##Requirement (2) Manipulate and clean data.
+df2 = pd.read_csv("NEO_Earth_Close_Approaches.csv", usecols=["Object","CA DistanceMinimum (km)"])
+df2.head()
+df2['CA DistanceMinimum (km)']= df2['CA DistanceMinimum (km)'].div(384,400)
+df2 = pd.DataFrame(df2.tail(10))
+
+## Create table
+table = ax.table(cellText=df2.values, colLabels=('Object', 'Lunar Distance'), colColours =["palegreen"] * 10, cellLoc ='center', loc='center')
+table.set_fontsize(12)
+table.scale(1,2)
+
+##display table
+fig.tight_layout()
+plt.show()
 
 #Pie chart of moon distance
 ##Requirement (4) Visualize your data. Make 2 basic plots with matplotlib.
@@ -112,7 +137,7 @@ fig1, ax1 = plt.subplots()
 plt.title('Ten closest Asteroids', fontsize = 15)
 plt.suptitle('The minimum possible close-approach distance in kilometers in comparison to the Moon distance.', fontsize = 15)
 ax1.axis('equal')
-plt.pie(df['CA DistanceMinimum (km)'], labels = df['Object'],textprops={"fontsize":10})
+plt.pie(df2['CA DistanceMinimum (km)'], labels = df['Object'],textprops={"fontsize":10})
 plt.show()
 
 #Moon diameter comparison
@@ -127,7 +152,7 @@ ax.axis('tight')
 
 df3 = pd.read_csv("NEO_Earth_Diameter.csv", usecols = ["Object", "Min Diameter"])
 df3.head()
-*Calculate the percent diameter compared to the Moon.
+#Calculate the percent diameter compared to the Moon.
 ##Requirement (2) Manipulate and clean data.
 df3['Min Diameter']= df3['Min Diameter'].div(3474000)*(100)
 df3 = pd.DataFrame(df3.tail(10))
@@ -152,7 +177,8 @@ plt.suptitle('Near Earth Asteroids Minimum Diameter in meters')
 plt.show()
 
 #display image
-
+#Near Earth Asteroid Tracking provides details for future endeavors that may lead
+#to ultimately saving the planet from future disasters.
 im = img.imread('PIA25329_small.jpg')
 plt.axis('off')
 plt.imshow(im)
